eef6659017e819cda1d814a5a2e2c2274b24c152
witchhunter <anil.kulkarni@gmail.com>
2018-07-13 11:44:54 +0530

    added package greeting

---
+++ code/basic/10_packages/greeting/greet.go
@@ -0,0 +1,13 @@
+package greeting
+
+import "fmt"
+
+func GoodMorning() {
+	fmt.Println("Good Morning, Octallium!")
+	fmt.Println()
+}
+
+func GoodNight(name string) {
+	fmt.Println("Good Night, ", name)
+	fmt.Println()
+}
---
+++ code/basic/10_packages/packages.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"../greeting"
+)
+
+func main() {
+	greeting.GoodMorning()
+	greeting.GoodNight("Gopher!")
+}
